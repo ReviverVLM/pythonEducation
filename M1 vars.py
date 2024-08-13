1b23c8100062b727192f8dfaebd3a6a2e0323ea8
ReviverVLM <hardssd@mail.ru>
2024-08-13 15:53:49 +0300

    Практика переменные

---
+++ M1 vars.py
@@ -0,0 +1,6 @@
+homework_tasks_count = 12
+hours_spent_on_homework_count = 1.5
+course_name = "Python"
+avg_time_on_task_hours = hours_spent_on_homework_count / homework_tasks_count
+main_string = "Курс: " + course_name + ", всего задач: " + str(homework_tasks_count) + ", часов выполнения: " + str(hours_spent_on_homework_count) + ", среднее время выполнения в часах: " + str(avg_time_on_task_hours)
+print(main_string)
