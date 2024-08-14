a6e92b4e808fd8cea26c9cfc031d88ca21ef3e0b
ReviverVLM <hardssd@mail.ru>
2024-08-14 17:42:05 +0300

    Корректировка

--- M1 dynamic typification.py
+++ M1 dynamic typification.py
@@ -1,8 +1,8 @@
 name = "ReviverVLM"
 age = 29
-new_age = age + 1
+age = age + 1
 is_student = True
 print("Name: " + name)
 print("Age: " + str(age))
-print("New_age: " + str(new_age))
+print("New_age: " + str(age))
 print("Is student: " + str(is_student))
