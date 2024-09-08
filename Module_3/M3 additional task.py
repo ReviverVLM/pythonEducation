a741d3443d8ea818c704a5d402c2a3dafc055ee8
ReviverVLM <hardssd@mail.ru>
2024-09-08 23:12:44 +0300

    M3 Улучшайзер(рефакторинг) обработки словаря

--- Module_3/M3 additional task.py
+++ Module_3/M3 additional task.py
@@ -25,25 +25,35 @@ print(result)
 99
 """
 
+# def dict_to_list(argument):
+#     dict_list = []
+#     for key in argument:
+#         dict_list.append(key)
+#         dict_list.append(argument[key])
+#     return dict_list
+#
+#
+# print(dict_to_list({'cube': 7, 'drum': 8}))
+"""Оптимизировано до *argument.items(). Без распаковки не работает. 
 
-def dict_to_list(argument):
-    dict_list = []
-    for key in argument:
-        dict_list.append(key)
-        dict_list.append(argument[key])
-    return dict_list
-
-
-print(dict_to_list({'cube': 7, 'drum': 8}))
+Пояснение нейросетки: 
+ Проблема заключается в том, что argument.items() возвращает объект dict_items, который не может быть непосредственно
+ распакован в sum_of_computer_types. Это приведет к ошибке, так как функция ожидает, что 
+ аргументы будут переданы в виде отдельных элементов, а не в виде итератора.
+ 
+ Вкратце isinstance(argument.items(), object) == True. Это итерируемый объект
+ 
+ Дополнение: ошибка не выдаётся, но пропускается
+"""
 
 
 def sum_of_computer_types(*args):
     result = 0
     for argument in args:
-        if isinstance(argument, tuple | list | set):
+        if isinstance(argument, (tuple, list, set)):
             result += sum_of_computer_types(*argument)
         elif isinstance(argument, dict):
-            result += sum_of_computer_types(dict_to_list(argument))
+            result += sum_of_computer_types(*argument.items())
         elif isinstance(argument, int):
             result += argument
         elif isinstance(argument, str):
@@ -52,11 +62,11 @@ def sum_of_computer_types(*args):
 
 
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-print(sum_of_computer_types([1, 2, 2], [2, 3]))
+# print(sum_of_computer_types([1, 2, 2], [2, 3]))
 print(sum_of_computer_types(*data_structure))
