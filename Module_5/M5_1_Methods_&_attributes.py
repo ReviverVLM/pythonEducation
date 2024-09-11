97bf27d9137ed9e8e6c471638895613a57377907
ReviverVLM <hardssd@mail.ru>
2024-09-11 21:11:22 +0300

    M5 методы и атрибуты, Задание 3

--- Module_5/M5_1_Methods_&_attributes.py
+++ Module_5/M5_1_Methods_&_attributes.py
@@ -1,3 +1,5 @@
+import time
+
 """
 Реализуйте класс House, объекты которого будут создаваться следующим образом:
 House('ЖК Эльбрус', 30)
@@ -14,7 +16,23 @@ Update: Второе задание в том же файле
 __len__(self) - должен возвращать кол-во этажей здания self.number_of_floors.
 __str__(self) - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>".
 """
-import time
+
+"""
+Update: Третье задание в том же файле
+Необходимо дополнить класс House следующими специальными методами:
+__eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
+Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать результаты
+сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
+__add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+__radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+Остальные методы арифметических операторов, где self - x, other - y:
+
+Следует заметить, что other может быть не только числом, но и вообще любым объектом другого класса.
+Для более точной логики работы методов __eq__, __add__  и других методов сравнения и арифметики перед выполняемыми
+действиями лучше убедиться в принадлежности к типу при помощи функции isinstance:
+isinstance(other, int) - other указывает на объект типа int.
+isinstance(other, House) - other указывает на объект типа House.
+"""
 
 
 class House:
@@ -26,7 +44,58 @@ class House:
         return self.number_of_floors
 
     def __str__(self):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+        about = f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+        return about
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors == other
+        elif isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors < other
+        elif isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors <= other
+        elif isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors > other
+        elif isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors >= other
+        elif isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors != other
+        elif isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        elif isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+            return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
 
     def go_to(self, new_floor):
         if new_floor == 1:
@@ -47,15 +116,15 @@ urban = House("Городская многоэтажка", 10)
 
 print(village.name, village.number_of_floors)
 print(urban.name, urban.number_of_floors)
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
-
-want_on_floor = 2
-village.go_to(want_on_floor)
-urban.go_to(want_on_floor)
-village.go_to(want_on_floor)
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
+#
+# want_on_floor = 2
+# village.go_to(want_on_floor)
+# urban.go_to(want_on_floor)
+# village.go_to(want_on_floor)
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -68,3 +137,35 @@ print(h2)
 # __len__
 print(len(h1))
 print(len(h2))
+""""""
+urban = 2 + urban
+print(urban)
+village += 1
+
+village = village + 6
+print(village)
+print("----------")
+""""""
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
