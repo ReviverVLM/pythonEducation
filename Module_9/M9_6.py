a992686604fb2b3880a55efe73dd6ef2537882e1
ReviverVLM <hardssd@mail.ru>
2024-11-17 10:12:30 +0300

    М9 Генераторы, переделано для получения требуемого результата

--- Module_9/M9_6.py
+++ Module_9/M9_6.py
@@ -7,23 +7,27 @@
 def all_variants(text):
     def path_string(string, start, end):
         return string[start:end]
-    for first in range(len(text)+1):
-        for second in range(len(text)+1):
+
+    for first in range(len(text) + 1):
+        for second in range(len(text) + 1):
             if second > first:
                 yield path_string(text, first, second)
 
 
-# def sorted_all_variants(text):
-#     def path_string(string, start, end):
-#         return string[start:end]
-#     for first in range(len(text)+1):
-#         for second in range(len(text)+1):
-#             if second > first:
-#                 yield path_string(text, first, second)
+def sorted_all_variants(text):
+    start = 0
+    end = 1
+    counter = 1
+    while counter <= len(text):
+        if start < end:
+            yield text[start:end]
+            start, end = start + 1, end + 1
+        if len(text) < end:
+            start = 0
+            counter += 1
+            end = counter
 
-# yield string
 
-
-result = all_variants("abcd")
+result = sorted_all_variants("abcd")
 for char in result:
     print(char)
