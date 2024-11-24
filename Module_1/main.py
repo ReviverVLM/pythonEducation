ac9d72cfe06b70ca90f98bd726c2894ec9e20596
ReviverVLM <hardssd@mail.ru>
2024-11-24 20:41:49 +0300

    М10 Очереди для обмена данными между потоками

--- Module_1/main.py
+++ Module_1/main.py
@@ -1,1 +1,8 @@
 print("Hello world")
+
+guests_names = [
+'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+for guest in enumerate(guests_names):
+    print(guest[1])
