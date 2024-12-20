d43ad55cc1c4c5656adeff73fe9213e2372cffc1
ReviverVLM <hardssd@mail.ru>
2024-12-20 21:37:19 +0300

    Функция eval()

--- Module_1/main.py
+++ Module_1/main.py
@@ -1,8 +1,23 @@
-print("Hello world")
+# print("Hello world")
 
-guests_names = [
-'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
-'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
-]
-for guest in enumerate(guests_names):
-    print(guest[1])
+# guests_names = [
+# 'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+# 'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+# ]
+# for guest in enumerate(guests_names):
+#     print(guest[1])
+
+# eval() with user input
+
+from math import *
+import time
+for l in range(1, 3):
+
+    a = 5
+    func = input("Enter Math Function to Evaluate:\n")
+    try:
+        print(eval(func))
+    except Exception as ex:
+        print(ex)
+        break
+print('Done')
